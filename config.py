891eb567401d08b5757fef4534349f2c6a23b9df
dpetek <dpetek1@gmail.com>
2021-06-10 23:22:13 -0700

    remove config

--- config.py
+++
@@ -1,4 +0,0 @@
-DB_USER = "root"
-DB_PASSWORD = "catu4anIIB1NdHjq"
-DB_DATABASE = "witts_new"
-DB_SQL_CONNECTION_NAME = "wits-wagers:us-central1:dpetek-witts"
--- main.py
+++ main.py
@@ -13,9 +13,5 @@ def index():
     return render_template('gen/index.html')
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # socketio.run(app)
     app.run(host='127.0.0.1', port=8080, debug=True)
 
